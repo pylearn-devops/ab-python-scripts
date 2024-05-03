f49ba097234cd06bd8f6eca79014b445eeb945ff
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2024-05-03 00:32:39 -0400

    Create dead_link_v2.py

---
+++ dead_links/dead_link_v2.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import re
+
+root_directory = "/Users/akashbasa/pylearn/dead_link_checker/"  # Change this to your root directory
+
+
+def find_links(filepath):
+    with open(filepath, "r", encoding="utf-8") as file:
+        content = file.read()
+        deep_links = re.findall(r'\[.*?\]\((?!http)(.*?)\)', content)  # Find deep links
+        all_links = re.findall(r'\[.*?\]\((https?://.*?)\)', content)  # Find http(s) links
+
+        return deep_links, all_links
+
+
+def check_link(link):
+    try:
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        driver = webdriver.Chrome(options=options)
+        driver.get(link)
+        return True
+    except:
+        return False
+    finally:
+        driver.quit()
+
+
+def find_broken_links(directory):
+    broken_links = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".md"):
+                filepath = os.path.join(root, file)
+                deep_links, all_links = find_links(filepath)
+                for link in all_links:
+                    print("link ------------->", link)
+                    if not check_link(link):
+                        broken_links.append((filepath, link))
+    return broken_links
+
+
+broken_links = find_broken_links(root_directory)
+
+if broken_links:
+    print("Broken Links:")
+    for link_info in broken_links:
+        print(f"File: {link_info[0]}, Broken Link: {link_info[1]}")
+else:
+    print("No broken links found.")
