211321018bd9655d048b944f7918b8c850cae4e7
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2024-05-29 23:03:37 -0400

    Update env.py

--- lambda-atdd/env.py
+++ lambda-atdd/env.py
@@ -3,7 +3,10 @@ def before_all(context):
     context.env = context.config.userdata.get('env', 'default')
     if context.env == 'prod':
         context.lambda_name = 'prod-lambda-function'
+        context.rule_name = 'prod-eventbridge-rule'
     elif context.env == 'staging':
         context.lambda_name = 'staging-lambda-function'
+        context.rule_name = 'staging-eventbridge-rule'
     else:
         context.lambda_name = 'dev-lambda-function'
+        context.rule_name = 'dev-eventbridge-rule'
